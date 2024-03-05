9b8dccd55f240dda36ec01ea8ecfb41e8aa4d159
Jewel-cse <md.jewel.cse@gmail.com>
2024-03-05 16:40:13 +0600

    explore @Arround

---
+++ src/main/java/com/jewel/learnspringAOP/AOPExamples/Aspects/PerformanceTrackingAspect.java
@@ -0,0 +1,30 @@
+package com.jewel.learnspringAOP.AOPExamples.Aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class PerformanceTrackingAspect {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Around("execution(* com.jewel.learnspringAOP.AOPExamples.*.*.*(..))")
+    public Object findExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        //start time
+        //proceed execution of method
+        //end time
+        //calculate performance
+
+        long startTimeMilis = System.currentTimeMillis();
+        Object returnValue = proceedingJoinPoint.proceed();
+        long endTimeMilis = System.currentTimeMillis();
+        long executionTime = endTimeMilis - startTimeMilis;
+
+        logger.info("Arround aspect - {} Method executed in {} ms",proceedingJoinPoint,executionTime);
+        return  returnValue;
+    }
+}
